e4de6daa91f169a5377bf66662b5ad2f67fd0f80
aman_codde <amandeepk221@gmail.com>
2025-04-05 15:11:04 -0700

    feat: create register form and add styles

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,38 @@
 const Register = () => {
     return(
-        <div>
-            <h1>Register page</h1>
+        <div className="form-container">
+            <h1>Register Form</h1>
+            <form className="form-group">
+                <div>
+                    <label>Username</label>
+                    <input
+                        type="text"
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input
+                        type="email"
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Create Password</label>
+                    <input
+                        type="password"
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Confirm Password</label>
+                    <input
+                        type="password"
+                        required
+                    />
+                </div>
+                <button type="submit">Register</button>
+            </form>
         </div>
     )
 };
