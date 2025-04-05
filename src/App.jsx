2e682a6535dd7a70ed623ef4c87d7452ff391ee9
aman_codde <amandeepk221@gmail.com>
2025-04-04 22:43:01 -0700

    feat: set up routing with BrowserRouter, Links and Routes for Register, Login and Dashboard pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
+import { BrowserRouter, Link, Route, Routes} from 'react-router-dom'
 import './App.css'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
 
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <div>
+        {/* Navigation */}
+          <nav>
+            <ul>
+              <li><Link to="/register">Register</Link></li>
+              <li><Link to="/login">Login</Link></li>
+              <li><Link to="/dashboard">Dashboard</Link></li>
+            </ul>
+          </nav>
+
+        {/* Routes */}
+        <Routes>
+          <Route path='/register' element={<Register/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
